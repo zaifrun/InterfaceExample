377f6312180e5e4c1fbb5530518b57cc16e97cd8
MAKN1444 <MAKN1444@L03859>
2025-08-26 10:14:15 +0200

    Add project files.

---
+++ MyContacts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+//collections : https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic?view=net-9.0
+
+namespace InterfaceExample
+{
+    public class  MyContacts
+    {
+        private ICollection<Contact> contacts;
+        public MyContacts(ICollection<Contact> contacts)
+        {
+            this.contacts = contacts;
+        }
+
+        public void AddContact(Contact contact)
+        {
+            this.contacts.Add(contact);
+        }
+
+        public int CountContacts { 
+            get { return contacts.Count(); }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using InterfaceExample;
+
+
+//brug af  : https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.icollection-1?view=net-9.0
+
+Console.WriteLine("Hello, World!");
+//MyContacts contacts = new MyContacts(new List<Contact>());
+//MyContacts contacts = new MyContacts(new LinkedList<Contact>());
+MyContacts contacts = new MyContacts(new HashSet<Contact>());
+
+
+
+var hans = new Contact(name: "hans hansen", "hansenvej 2", "1234");
+var jens = new Contact(name: "jens jensen", "jensenvej 4", "5678");
+contacts.AddContact(hans);
+contacts.AddContact(jens);
+
+Console.WriteLine("Antal kontakter: " + contacts.CountContacts);
+Console.ReadLine(); //end when key pressed
+
